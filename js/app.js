091f0fd429b2273a02f03d81d28de9746303a09a
Krunal Patel <kp_ptl@yahoo.com>
2014-01-02 16:07:08 +0530

    Navigation auto scroll disabled, generic html for all the pages

--- js/app.js
+++ js/app.js
@@ -1,17 +1,83 @@
 $(document).ready( function() {
+    
+	$.fn.smint = function( options ) {
+
+		// adding a class to users div
+		$(this).addClass('smint')
+
+		var settings = $.extend({
+		            'scrollSpeed '  : 500
+		}, options);
+
+		//Set the variables needed
+		//var optionLocs = new Array();
+		var lastScrollTop = 0;
+		var menuHeight = $(".smint").height();
+
+		return $('.smint a').each( function(index) {
+            
+			if ( settings.scrollSpeed ) {
+				var scrollSpeed = settings.scrollSpeed
+			}
+
+
+			var stickyTop = $('.smint').offset().top;
+				
+
+			// check position and make sticky if needed
+			var stickyMenu = function(direction){
+
+				// current distance top
+				var scrollTop = $(window).scrollTop(); 
+
+				//console.log('Sticky Top:' + stickyTop);
+				//console.log('Scroll Top:' + scrollTop);
+				
+				// if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
+				if (scrollTop > stickyTop) { 
+					$('.smint').css({ 'position': 'fixed', 'top':0 }).addClass('fxd');	
+					
+				} else if(scrollTop < 100){
+					
+					$('.smint').css({ 'position': 'absolute', 'top':'100px' }).removeClass('fxd'); 
+					
+				}   
+
+			};
+
+			// run functions
+			stickyMenu();
+
+			// run function every time you scroll
+			$(window).scroll(function() {
+				//Get the direction of scroll
+				var st = $(this).scrollTop();
+				if (st > lastScrollTop) {
+				    direction = "down";
+				} else if (st < lastScrollTop ){
+				    direction = "up";
+				}
+				lastScrollTop = st;
+				stickyMenu(direction);
+
+			});
+        	
+		});
+	};
+
+
+
+
     $('.subMenu').smint({
     	'scrollSpeed' : 500
     });
-   
-	$('.banner').unslider({
-		speed: 300,               //  The speed to animate each slide (in milliseconds)
-		delay: 3000,              //  The delay between slide animations (in milliseconds)
-		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-		dots: true,               //  Display dot navigation
-		fluid: false              //  Support responsive design. May break non-responsive designs
+
+
+    /*Footer*/
+
+    $("#footer-lower").hide();
+	$("footer").click(function(){
+		$("#footer-lower").toggle();
 	});
-
-	
-
 	
 });
